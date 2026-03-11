90e6bc1567f24fdfce661e6c65c741ab4b6825d6
MRDrewiena1 <kontad.dr@gmail.com>
2026-03-11 11:08:26 +0100

    Add CalculateAverage

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -5,3 +5,14 @@ Console.WriteLine("Hello, World!");
 
 
 Console.WriteLine("Hello, World!");
+
+static double CalculateAverage(int[] values)
+{
+    var sum = 0.0;
+    var count = values.Length;
+    foreach(var i in values)
+    {
+        sum += i;
+    }
+    return sum / count;
+}
